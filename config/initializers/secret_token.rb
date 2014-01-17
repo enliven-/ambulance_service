cf98c48c0b65b0d243685192c720d87a6248a68d
Viksit Arora <viksit@air.(none)>
2014-01-17 23:04:43 +0530

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmbulanceService::Application.config.secret_key_base = '12dace4381d40e0b58271d1b1c10bb8dc69db1f84a6b235eecbae596eb8633368a27383b7853876b74c6ac70f081488f8ae8b1ef4357d15cef4e5adbd3ec16fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmbulanceService::Application.config.session_store :cookie_store, key: '_ambulance_service_session'
